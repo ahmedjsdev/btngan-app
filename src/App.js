4f3636fc89f29d8a2b7ca34327ddcba38f44efb5
ahmedjsdev <ahmedjsdev@gmail.com>
2022-10-12 01:07:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Contact from "./components/Contact";
+import HomePage from "./components/homePage";
+import Navbar from "./components/Navbar";
+import ProductDetails from "./components/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="product/:slug" element={<ProductDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
